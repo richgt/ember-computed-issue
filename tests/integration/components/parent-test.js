15cab547197cce71a5b8ff74d7213f57665d8cd1
Robert Jackson <rjackson@linkedin.com>
2020-06-25 12:27:37 -0400

    Add basic functionality test.

--- tests/integration/components/parent-test.js
+++ tests/integration/components/parent-test.js
@@ -1,26 +1,30 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | parent', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders initially', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<Parent />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.querySelector('[data-test-parent-value]').textContent.trim(), 'Hello|World');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <Parent>
-        template block text
-      </Parent>
-    `);
+  test('it can update when child changes CP dependent keys', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(hbs`<Parent />`);
+
+    assert.equal(this.element.querySelector('[data-test-parent-value]').textContent.trim(), 'Hello|World', 'precond');
+
+    await fillIn('.child-a', 'foo');
+
+    assert.equal(this.element.querySelector('[data-test-parent-value]').textContent.trim(), 'foo|World', 'updated value');
   });
 });
